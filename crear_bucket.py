1f93d840964904603e5ec418b6fcc637abe79cbd
Paulo MiraBarr <paulo.miranda@utec.edu.pe>
2025-11-02 22:58:18 -0500

    Add files via upload

---
+++ crear_bucket.py
@@ -0,0 +1,20 @@
+import boto3
+
+def lambda_handler(event, context):
+    # Entrada (json)
+    nombre_bucket = event['body']['bucket']
+
+    # Proceso
+    s3 = boto3.client('s3')
+    region = s3.meta.region_name or 'us-east-1'
+    params = {'Bucket': nombre_bucket}
+    if region != 'us-east-1':
+        params['CreateBucketConfiguration'] = {'LocationConstraint': region}
+
+    s3.create_bucket(**params)
+
+    return {
+        'statusCode': 201,
+        'bucket': nombre_bucket,
+        'region': region
+    }
